db3f124910d8aef5abc482d31bdb433e44d96aa4
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2026-02-15 13:16:48 -0300

    Update Usuario.java

--- src/main/java/com/gerenciamento/tarefas/model/Usuario.java
+++ src/main/java/com/gerenciamento/tarefas/model/Usuario.java
@@ -4,12 +4,10 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
