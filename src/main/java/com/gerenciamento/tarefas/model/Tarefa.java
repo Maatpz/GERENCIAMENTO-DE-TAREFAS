568de1bfea8670a35971c64380837383fbac9f9b
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2026-02-15 13:16:45 -0300

    Update Tarefa.java

--- src/main/java/com/gerenciamento/tarefas/model/Tarefa.java
+++ src/main/java/com/gerenciamento/tarefas/model/Tarefa.java
@@ -2,11 +2,9 @@ package com.gerenciamento.tarefas.model;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedDate;
+
 import org.springframework.data.annotation.LastModifiedDate;
 
 import com.gerenciamento.tarefas.model.enums.Priority;
@@ -21,7 +19,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
