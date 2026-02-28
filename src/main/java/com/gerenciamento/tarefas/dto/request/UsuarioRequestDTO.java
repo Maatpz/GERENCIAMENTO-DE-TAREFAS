1bbe702dd33da4ac652c1e9e5e7117ec32edeed7
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2026-02-28 13:06:33 -0300

    Create UsuarioRequestDTO.java

---
+++ src/main/java/com/gerenciamento/tarefas/dto/request/UsuarioRequestDTO.java
@@ -0,0 +1,16 @@
+package com.gerenciamento.tarefas.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UsuarioRequestDTO(
+
+                @NotBlank(message = "O nome é obrigatório") String name,
+
+                @NotBlank(message = "O email é obrigatório") @Email(message = "Email inválido") String email,
+
+                @NotBlank(message = "A senha é obrigatória") @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres") String password
+
+) {
+}
